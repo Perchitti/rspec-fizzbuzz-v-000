ca87e21a934bc5b843723eb83a699ca9dc443663
Lauren Perchitti <laurenperchitti@gmail.com>
2018-02-21 20:23:53 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(x)
   if (x % 3) == 0 && (x % 5) == 0
     puts "FizzBuzz"
-  elsif (x % 5) == fizzbuzz
+  elsif (x % 5) == 0
     puts "Buzz"
-  elsif (x % 3) == fizzbuzz
+  elsif (x % 3) == 0
     puts "Fizz"
  end
 end
